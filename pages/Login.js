d496c9c43ca5143b452dd617c65237ae4f3fecb8
Majd alali <m-syr@outlook.sa>
2023-04-18 20:59:50 +0800

    delete
    - auth

--- pages/Login.js
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router";
-import Login from "../pages/auth/login";
-
-export default function LoginPage() {
-  const router = useRouter();
-  return <Login router={router} />;
-}
